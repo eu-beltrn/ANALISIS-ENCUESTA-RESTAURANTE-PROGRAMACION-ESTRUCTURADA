f77de67ff793fb2aba1f3d0b9db30d6bdca7c9ba
AmayaNicole <amayanicole644@gmail.com>
2026-04-30 11:28:11 -0600

    Subiendo reportes 6-10

--- reportes.py
+++ reportes.py
@@ -5,7 +5,32 @@
 # ==============================================================================
 # REPORTES 6 AL 10 - Nicole
 # ==============================================================================
+class AnalizadorEncuesta:
+    def __init__(self, lista_objetos):
+        self.encuestados = lista_objetos # Esta es la lista 'datos' del main
 
+    def reporte_6(self):
+        acum, cont = {}, {}
+        for e in self.encuestados:
+            acum[e.comida_preferida] = acum.get(e.comida_preferida, 0) + e.gasto
+            cont[e.comida_preferida] = cont.get(e.comida_preferida, 0) + 1
+        return {c: acum[c] / cont[c] for c in acum}
+
+    def reporte_7(self):
+        return sum(e.producto for e in self.encuestados) / len(self.encuestados)
+
+    def reporte_8(self):
+        return sum(e.servicio for e in self.encuestados) / len(self.encuestados)
+
+    def reporte_9(self):
+        # Ahora usamos el atributo del objeto
+        return sum(e.calificacion_general for e in self.encuestados) / len(self.encuestados)
+
+    def reporte_10(self):
+        conteo = {}
+        for e in self.encuestados:
+            conteo[e.precio] = conteo.get(e.precio, 0) + 1
+        return conteo
 # ==============================================================================
 # REPORTES 11 AL 15 - Jonathan
 # ==============================================================================
